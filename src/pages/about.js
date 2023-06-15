b6a0a976e9aab868637877043098fdd19585c874
Emanuel Garcia <wemagw@gmail.com>
2023-06-15 00:02:49 -0300

    About section error fixed

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,7 +11,7 @@ const AnimatedNumbers = ({value}) => {
 
     const motionValue = useMotionValue(0);
     const springValue = useSpring(motionValue, { duration: 3000});
-    const isInView = useInView(ref);
+    const isInView = useInView(ref, {once: true});
 
     useEffect(() => {
         if(isInView) {
@@ -53,16 +53,16 @@ const about = () => {
                             Biography
                         </h2>
                         <p className='font-medium'>
-                            Hi, I'm CodeBucks, a web developer and UI/UX designer with a passion for creating beautiful, functional, 
+                            Hi, I&apos;m CodeBucks, a web developer and UI/UX designer with a passion for creating beautiful, functional, 
                             and user-centered digital experiences. With 4 years of experience in the field. I am always looking for 
-                            new and innovative ways to bring my clients' visions to life. 
+                            new and innovative ways to bring my clients&apos; visions to life. 
                         </p>
                         <p className='font-medium my-4'>
-                            I believe that design is about more than just making things look pretty – it's about solving problems and 
+                            I believe that design is about more than just making things look pretty – it&apos;s about solving problems and 
                             creating intuitive, enjoyable experiences for users. 
                         </p>
                         <p className='font-medium'>
-                            Whether I'm working on a website, mobile app, or 
+                            Whether I&apos;m working on a website, mobile app, or 
                             other digital product, I bring my commitment to design excellence and user-centered thinking to 
                             every project I work on. I look forward to the opportunity to bring my skills and passion to your next project.
                         </p>
@@ -91,7 +91,7 @@ const about = () => {
                         </div>
                         <div className='flex flex-col items-end justify-center'>
                             <span className='inline-block text-6xl font-bold'>
-                                40+
+                                <AnimatedNumbers value={40} />
                             </span>
                             <h2 className='text-xl font-medium capitalize text-dark/75'>
                                 projects completed
@@ -99,7 +99,7 @@ const about = () => {
                         </div>
                         <div className='flex flex-col items-end justify-center'>
                             <span className='inline-block text-6xl font-bold'>
-                                4+
+                                <AnimatedNumbers value={4} />
                             </span>
                             <h2 className='text-xl font-medium capitalize text-dark/75'>
                                 years of experience
