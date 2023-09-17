6e7dd5ec213c69547186cc1de97534b4f83602d0
Emanuel Garcia <wemagw@gmail.com>
2023-09-16 23:05:51 -0300

    button responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -178,8 +178,10 @@ const Navbar = () => {
             <button
               onClick={() => setMode(mode === "light" ? "dark" : "light" )}
               className={`
-                ml-3 flex items-center justify-center sm:mx-1
-                rounded-full p-1 ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark" }
+                ml-3 flex items-center justify-center 
+                rounded-full p-1 
+                sm:w-8 sm:h-8 sm:ml-1
+                ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark" }
               `}
             >
               {
