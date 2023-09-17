aee369fd79f8cd844d49bb6747407a8616267294
Emanuel Garcia <wemagw@gmail.com>
2023-09-16 23:23:09 -0300

    button fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -58,7 +58,7 @@ export default function Home() {
                     rounded-lg text-lg font-semibold hover:text-dark dark:text-dark
                     border-2 border-solid border-transparent hover:border-dark
                     hover:dark:bg-dark hover:dark:text-light hover:dark:border-light
-                    sm:p-2 sm:px-4 sm:text-base  // Clases específicas para pantallas pequeñas
+                    sm:w-28 sm:h-12 sm:text-base  
                   "
                   download={true}
                 >
